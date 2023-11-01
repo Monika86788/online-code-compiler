1f6adcf320f67b4cb30861f00a42569c827fabeb
Monika86788 <109470244+Monika86788@users.noreply.github.com>
2023-11-02 00:26:14 +0530

    Add files via upload

---
+++ defaultStubs.js
@@ -0,0 +1,12 @@
+const stubs={};
+stubs.cpp=`#include <iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+}
+`;
+stubs.py=`print("hey")
+`;
+export default stubs;
